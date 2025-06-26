c29be148bcfafb129d0685f3a4e94ea9bace3b0c
Sgal Cheung <zhangsigao@live.com>
2025-06-26 14:20:43 +0800

    fix: update Supabase client initialization to use SUPABASE_KEY instead of SUPABASE_ANON_KEY

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-supabase-auth.vercel.app",
+  site: "https://sgalculture-astro-supabase.netlify.app",
   output: "server",
-  adapter: vercel(),
   integrations: [tailwind(), solidJs()],
+  adapter: netlify(),
 });
